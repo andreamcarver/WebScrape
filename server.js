dbbfcfaa31ba2785f2d5782f0e9c0ff41410f1d5
andrea.m.carver@gmail.com <andrea.m.carver@gmail.com>
2018-08-10 13:58:44 -0500

    Edited scrape routes, added models

--- server.js
+++ server.js
@@ -35,33 +35,74 @@ app.get("/", function(req, res) {
   res.send("index.html");
 });
 
-// Scrape Route
 app.get("/scrape", function(req, res) {
-  console.log("The goose is scraping");
-  // axios performs a similar function as either request/fetch
-  axios.get("https://www.reddit.com/").then(function(response) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    // reddit span y8HYJ-y_lTUHkQIc1mdCq
-    $("span h2").each(function(i, element) {
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
       var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        // .children("a")
+        .children("a")
         .text();
       result.link = $(this)
         .children("a")
         .attr("href");
 
+      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          console.log("Articles:" + dbArticle);
+          // View the added result in the console
+          console.log(dbArticle);
         })
         .catch(function(err) {
+          // If an error occurred, send it to the client
           return res.json(err);
         });
     });
-    res.send("Scrape Successful!");
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
   });
 });
+
+// Scrape Route
+// app.get("/scrape", function(req, res) {
+//   console.log("The goose is scraping");
+//   // axios performs a similar function as either request/fetch
+//   axios.get("http://www.echojs.com/").then(function(response) {
+//     // Then, we load that into cheerio and save it to $ for a shorthand selector
+//     var $ = cheerio.load(response.data);
+
+//     // Now, we grab every h2 within an article tag, and do the following:
+//     $("article h2").each(function(i, element) {
+//       // Save an empty result object
+//       var result = {};
+
+//       // Add the text and href of every link, and save them as properties of the result object
+//       result.title = $(this)
+//         .children("a")
+//         .text();
+//       result.link = $(this)
+//         .children("a")
+//         .attr("href");
+
+//       db.Article.create(result)
+//         .then(function(dbArticle) {
+//           console.log("Articles:" + dbArticle);
+//         })
+//         .catch(function(err) {
+//           return res.json(err);
+//         });
+//     });
+//     res.send("Scrape Successful!");
+//   });
+// });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
@@ -85,6 +126,46 @@ app.get("/articles", function(req, res) {
   db.Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
